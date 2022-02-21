f859d8ca25efef5c7c8cc3f5c553556e82333983
IbrokhimJalolov <85637066+ibrokhimjalolov@users.noreply.github.com>
2022-02-21 18:08:13 +0500

    Update admin.py

--- models/admin.py
+++ models/admin.py
@@ -1,14 +1,1 @@
 from django.contrib import admin
-
-from .models import (
-    Author,
-    Category,
-    Book,
-    User
-)
-
-
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(Book)
-admin.site.register(User)
